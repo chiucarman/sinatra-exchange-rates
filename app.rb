b28cdc794d936a9696ae8657fd40a8cf05871e80
Carman Chiu <101212528+chiucarman@users.noreply.github.com>
2023-09-19 03:27:24 +0000

    print symbols_url raw response

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+  symbols_url = "https://api.exchangerate.host/symbols"
+  resp = HTTP.get(symbols_url)
+  pp raw_reponse = resp.to_s
+  
   erb(:hello)
 end
 
